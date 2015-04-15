757a518db9b31e4076a34a2ed55f17212fb663ac
Jannik Franz <jannik.franz@hp.com>
2015-04-15 11:19:10 +0200

    Gave routine explanation to main. Created files for the functions

---
+++ C/RSA.h
@@ -0,0 +1,7 @@
+
+#ifndef __RSA_H_INCLUDED__
+#define __RSA_H_INCLUDED__
+
+
+
+#endif // __RSA_H_INCLUDED__
---
+++ C/main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+
+
+/**
+ * Main routine containing the following functionality
+ *	- Support the user in generating large prime numbers p and q
+ *  - Asks the user for a public exponent e
+ *  - Calculates and prints the private exponent d
+ *  - Request the user to enter a message M
+ *  - Encrypts the message into a ciphertext C and prints C
+ *  - Decrypts the ciphertext and prints the decrypted message M'
+ *  - Requests the user for the next message M until an empty message is entered.
+ */
+int main(int argc, char **argv) {
+	
+	return 0;
+}
